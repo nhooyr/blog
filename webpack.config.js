5621c8d145e69fcded9b2fe3c83a05d0f297c660
Anmol Sethi <hi@nhooyr.io>
2019-05-16 11:32:04 -0400

    Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }]
+  }
+}
