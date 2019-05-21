9d31d458e0b420a5fbaf5049485ea28009aff45c
Anmol Sethi <hi@nhooyr.io>
2019-05-21 16:03:54 -0400

    Switch to yarn pnp and vanilla create-react-app as postcss is supported

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = (props) => {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header css={{
-        width: "100%",
-        height: "1000px",
-        backgroundColor: "green"
-      }}>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-setInterval(() => {
-  console.log("render")
-  ReactDOM.render(<App />, document.getElementById('root'));
-}, 1000)
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
