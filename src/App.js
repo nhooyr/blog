b5114d0d8b5d73c60bf587cb1fd5a9373e537f11
Anmol Sethi <hi@nhooyr.io>
2019-05-21 09:51:06 -0400

    Emotion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" css={{
+      backgroundColor: 'green'
+    }} >
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
