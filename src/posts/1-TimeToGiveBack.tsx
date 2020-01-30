fd54e4440d05ba5998f8e0ca3a1093939b47a029
Anmol Sethi <hi@nhooyr.io>
2020-01-29 23:57:35 -0600

    Improve favicon

--- src/posts/1-TimeToGiveBack.tsx
+++ src/posts/1-TimeToGiveBack.tsx
@@ -4,12 +4,10 @@ import P from "../components/P"
 export default function BeginningMyBlog() {
   return (
     <>
-      <P>Coming soon....</P>
-      {/*<P>Hi, I'm Anmol. I go by nhooyr on the internet. I'm a software engineer from Toronto.</P>*/}
-      {/*<P>*/}
-      {/*  I've always wanted to write a blog and I'm really happy to finally be doing it. For my first blog post, I wanted*/}
-      {/*  to elaborate on myself, why I'm beginning a blog and what you can expect on here.*/}
-      {/*</P>*/}
+      <P>Hi, I'm Anmol. I go by nhooyr on the internet. I'm a software engineer from Toronto.</P>
+      <P>
+        I'm fairly young as far as experienced software engineers go
+      </P>
     </>
   )
 }
