c7719ab59a5749ad00a37b07a512b2ff1b54eff0
Anmol Sethi <hi@nhooyr.io>
2020-02-01 20:50:08 -0600

    Ensure TimeToGiveBack is not available on prod but I can push changes

--- src/posts/1-TimeToGiveBack.tsx
+++ src/posts/1-TimeToGiveBack.tsx
@@ -1,15 +1,18 @@
 import React from "react"
+import Link from "../components/Link"
 import P from "../components/P"
 
 export default function TimeToGiveBack() {
+  if (process.env.NODE_ENV === "production") {
+    return <P>Coming soon...</P>
+  }
   return (
     <>
-      <P>Coming soon...</P>
-      {/*<P>Hi, I'm Anmol. I go by nhooyr on the internet. I'm a software engineer from Toronto.</P>*/}
-      {/*<P>*/}
-      {/*  The last three years of my life have been the most wonderful and unexpected. In I dropped out of high school in*/}
-      {/*  Grade 12 to take a full time software engineering position at <Link href="https://coder.com">Coder</Link>. I*/}
-      {/*</P>*/}
+      <P>Hi, I'm Anmol. I go by nhooyr on the internet. I'm a software engineer from Toronto.</P>
+      <P>
+        The last three years of my life have been the most wonderful and unexpected. In I dropped out of high school in
+        Grade 12 to take a full time software engineering position at <Link href="https://coder.com">Coder</Link>. I
+      </P>
     </>
   )
 }
