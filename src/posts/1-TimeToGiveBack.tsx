cd95ffa896aba40328a8beba917e211219fe6d0c
Anmol Sethi <hi@nhooyr.io>
2020-02-09 02:06:38 -0500

    Update TimeToGiveBack

--- src/posts/1-TimeToGiveBack.tsx
+++ src/posts/1-TimeToGiveBack.tsx
@@ -6,7 +6,7 @@ export default function TimeToGiveBack() {
   return (
     <>
       <P>Hi, I'm Anmol. I go by nhooyr on the internet. I'm a software engineer from Toronto.</P>
-      <P>I am a self taught engineer. I did not attend university and I did not graduate high school.</P>
+      <P>I'm a self taught engineer. I didn't attend university and I didn't graduate high school.</P>
       <P>
         In July, 2017, at the end of my junior year of high school I became employed as a full time engineer at{" "}
         <Link href="https://coder.com">Coder</Link>. As I was able to gain employment during high school, I felt there
@@ -14,17 +14,16 @@ export default function TimeToGiveBack() {
       </P>
       <P>
         The greatest factor in my success as an engineer has been my own passion, initiative and focus. However, the
-        incredible volume of quality reading online in websites, blogs and forums has contributed immensely. For at
+        incredible volume of quality reading available on websites, blogs and forums has contributed immensely. For at
         least the last 8 years, the internet has been my primary means of education.
       </P>
-      <P>It is time for me to contribute back.</P>
+      <P>It's time for me to contribute back.</P>
       <P>
         On this blog you can expect to find quality writings about software, philosophy, life and everything in between.
         There will never be <i>any</i> ads, popups or bullshit.
       </P>
       <P>
-        From a technical standpoint, I believe this blog is well engineered and one of my first posts will be to justify
-        its inner workings.
+        This blog is well engineered and one of my first posts will be to justify its inner workings.
       </P>
       <P>I sincerely hope my efforts with this blog are successful in giving back.</P>
     </>
